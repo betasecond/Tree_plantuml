1e4e4e1a7ce64b54a2b43a2a96efca3979b66fb6
betasecoond <nbdhc9039@gmail.coom>
2024-02-18 20:59:58 +0800

    md 初步完成

--- internal/tree/formats/md.go
+++ internal/tree/formats/md.go
@@ -1,1 +1,24 @@
 package formats
+
+import (
+	"Tree_plantuml/internal/tree"
+	"strings"
+)
+
+// GenerateMDOutput 接收一个 FolderNode 根节点，然后生成其Markdown表示形式
+func GenerateMDOutput(node tree.FolderNode, level int) string {
+	var builder strings.Builder
+	indent := strings.Repeat("    ", level) // Markdown 使用空格进行缩进
+
+	if node.IsDir {
+		builder.WriteString(indent + "- " + node.Name + "\n") // 目录以列表形式表示
+	} else {
+		builder.WriteString(indent + "    - " + node.Name + "\n") // 文件作为列表项
+	}
+
+	for _, child := range node.Children {
+		builder.WriteString(GenerateMDOutput(child, level+1)) // 递归处理子节点
+	}
+
+	return builder.String()
+}
