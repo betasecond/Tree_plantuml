766d05b0ff1e2499c0f1561df9341c7a44f0f613
betasecoond <nbdhc9039@gmail.coom>
2024-02-18 20:59:11 +0800

    txt  初步完成

--- internal/tree/formats/txt.go
+++ internal/tree/formats/txt.go
@@ -2,6 +2,7 @@ package formats
 
 import (
 	"Tree_plantuml/internal/tree"
+	"fmt"
 	"strings"
 )
 
@@ -11,10 +12,13 @@ func GenerateTXTOutput(node tree.FolderNode, level int) string {
 	indent := strings.Repeat("  ", level)
 
 	if node.IsDir {
+		builder.WriteString(fmt.Sprint("%s[%s]\n", indent, node.Name))
 	} else {
+		builder.WriteString(fmt.Sprintf("%s%s\n", indent, node.Name))
 	}
 
 	for _, child := range node.Children {
+		builder.WriteString(GenerateTXTOutput(child, level+1)) // 递归处理子节点
 	}
 
 	return builder.String()
