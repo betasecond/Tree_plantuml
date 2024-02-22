83585f1532204aee2daec0b30fdc367dce3ab506
betasecoond <nbdhc9039@gmail.coom>
2024-02-22 23:09:10 +0800

    add config‘s parser:config.go

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,44 @@
 package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+// Config 定义了配置文件的结构
+type Config struct {
+	SourcePath string `json:"sourcePath"`
+	Features   struct {
+		ContextMenu      bool     `json:"contextMenu"`
+		ClipboardSupport bool     `json:"clipboardSupport"`
+		OutputFormats    []string `json:"outputFormats"`
+	} `json:"features"`
+	OutputDirectory string `json:"outputDirectory"`
+	RegisterKeys    []struct {
+		Name    string   `json:"name"`
+		Command string   `json:"command"`
+		Formats []string `json:"formats"`
+	} `json:"registerKeys"`
+}
+
+// LoadConfig 从指定路径加载配置文件
+func LoadConfig(configPath string) (*Config, error) {
+	configFile, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer configFile.Close()
+
+	bytes, err := ioutil.ReadAll(configFile)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := json.Unmarshal(bytes, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
